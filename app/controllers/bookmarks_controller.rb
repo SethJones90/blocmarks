bdd9bf8f4fffe6283568b73ac81615961c684fc8
Seth Jones <sethjmusic@gmail.com>
2015-08-24 09:17:23 -0400

    added bookmark crud

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,27 +1,56 @@
 class BookmarksController < ApplicationController
 
   def show
+    @topic = Topic.find(params[:topic_id])
     @bookmark = Bookmark.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @bookmark = Bookmark.new
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     @bookmark = Bookmark.new(params.require(:bookmark).permit(:url))
+    @bookmark.topic = @topic
     if @bookmark.save
       flash[:notice] = "This bookmark was successfully saved."
-      redirect_to @bookmark
+      redirect_to [@topic, @bookmark]
     else
       flash[:error] = "There was an error saving this bookmark. Please try again."
     end
   end
 
   def destroy
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+
+    if @bookmark.destroy
+      flash[:notice] = "Bookmark was successfully deleted."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the bookmark. Please try again."
+      render :show
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+
+    if @bookmark.update_attributes(params.require(:bookmark).permit(:url))
+      flash[:notice] = "Bookmark was updated"
+      redirect_to [@topic, @bookmark]
+    else
+      flash[:error] = "There was an error updating the bookmark. Please try again."
+      render :new
+    end
 
   end
 end
